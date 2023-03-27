ee071a728814003ddbc16d1e05373b0ee8909e99
akshaychavan010101 <akshaychavan010101@gmail.com>
2023-03-27 17:10:00 +0530

    check

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 require("dotenv").config();
 const redis = require("redis");
 const client = redis.createClient();
+client.on("error", (err) => console.log(err.message));
+(async () => await client.connect())();
+client.on("redy", () => console.log("Redis connected"));
 
 const { connection } = require("./config/db");
 const { UserRouter } = require("./routes/users.routes");
@@ -20,9 +23,6 @@ app.use(UserRouter);
 app.listen(process.env.PORT, async (req, res) => {
   try {
     await connection;
-    client.on("error", (err) => console.log(err.message));
-    (async () => await client.connect())();
-    client.on("redy", () => console.log("Redis connected"));
     console.log("Running and connected to DB");
   } catch (error) {
     console.log(error);
